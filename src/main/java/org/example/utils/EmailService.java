56fae4b890f48576819edd6e84689ad52af4489a
Vinh <vinhdangquang1301@gmail.com>
2024-10-28 00:14:06 +0700

    config send email

---
+++ src/main/java/org/example/utils/EmailService.java
@@ -0,0 +1,26 @@
+package org.example.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+    @Value("${mail.username}")
+    private String fromEmail;
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(fromEmail);
+
+        mailSender.send(message);
+    }
+}
